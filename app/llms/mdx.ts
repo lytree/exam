34c6b6595f1a25849e6311830fc5ff0c65a64d41
yangzh <yangzh@windit.com.cn>
2026-04-24 11:23:09 +0800

    del vitepress

--- app/llms/mdx.ts
+++ app/llms/mdx.ts
@@ -10,7 +10,7 @@ export async function loader({ params }: Route.LoaderArgs) {
     }
     return new Response(await getLLMText(page), {
         headers: {
-            'Content-Type': 'text/markdown',
+            'Content-Type': 'text/markdown; charset=utf-8',
         },
     });
 }
